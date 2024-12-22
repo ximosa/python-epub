1e02a5e0f08f11da5b4fd87a7e6a0f373d513bfb
Joaquin Saez <ximosa@gmail.com>
2024-12-23 00:30:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,7 @@ def generar_resumen(texto):
     Actúa como un lector profundo y reflexivo. Escribe el texto en primera persona, como si tú hubieras vivido la experiencia o reflexionado sobre los temas presentados.
     Sigue estas pautas:
     - Pon un titulo llamativo.
+    - Genera texto plano sobre.No incluyas etiquetas HTML de encabezado.
     - Reescribe el siguiente texto utilizando tus propias palabras, y asegúrate de mantener una longitud similar al texto original.
     No reduzcas la información, e intenta expandir cada punto si es posible.
     No me generes un resumen, quiero un texto parafraseado y expandido con una longitud comparable al texto original.
@@ -89,7 +90,6 @@ def generar_resumen(texto):
     - Usa un lenguaje claro y directo.
     - Escribe como si estuvieras narrando una historia.
     - Separa ideas solo con puntos y comas.
-    - No quiero encabezados solo texto
     -Importante, el texto debe adaptarse para que el lector de voz de google lo lea lo mejor posible
     
     Texto a resumir:
